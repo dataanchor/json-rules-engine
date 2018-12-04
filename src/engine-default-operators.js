4ef7f0cece4a85fee4afdad00137299f28defd02
Sreeraj Krishnan <sreerajkrishnan.k@gmail.com>
2018-12-04 21:15:36 +0530

    Added two new operators

--- src/engine-default-operators.js
+++ src/engine-default-operators.js
@@ -4,12 +4,35 @@ import Operator from './operator'
 
 let Operators = []
 Operators.push(new Operator('equal', (a, b) => a === b))
+Operators.push(new Operator('equalsTo', (a, b) => a == b))
 Operators.push(new Operator('notEqual', (a, b) => a !== b))
 Operators.push(new Operator('in', (a, b) => b.indexOf(a) > -1))
 Operators.push(new Operator('notIn', (a, b) => b.indexOf(a) === -1))
 
 Operators.push(new Operator('contains', (a, b) => a.indexOf(b) > -1, Array.isArray))
 Operators.push(new Operator('doesNotContain', (a, b) => a.indexOf(b) === -1, Array.isArray))
+Operators.push(new Operator('equalList', (a, b) => {
+  if (!b)
+        return false;
+
+    // compare lengths - can save a lot of time 
+    if (a.length != b.length)
+        return false;
+
+    for (var i = 0, l=a.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (a[i] instanceof Array && b[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!a[i].equals(b[i]))
+                return false;       
+        }           
+        else if (a[i] != b[i]) { 
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;   
+        }           
+    }       
+    return true;
+}, Array.isArray))
 
 function numberValidator (factValue) {
   return Number.parseFloat(factValue).toString() !== 'NaN'
